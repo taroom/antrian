7a4ffee2d28b53cf5ed7d458e120c2b488c80a85
sultanroomcode <sultanroomcode@gmail.com>
2021-05-03 20:45:03 +0700

    add html elements on index file

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
 include 'konfigurasi/database.php';
 include 'konfigurasi/fungsi.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
 
-include 'halaman/index.php';
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Antrian</title>
+</head>
+
+<body>
+    <?php
+    include 'halaman/index.php';
+    ?>
+</body>
+
+</html>
