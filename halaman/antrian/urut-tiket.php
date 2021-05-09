29a47b1c51d9e387cd1f0a7716c359cbce7c047f
sultanroomcode <sultanroomcode@gmail.com>
2021-05-10 00:37:41 +0700

    viewing all tiket

--- halaman/antrian/urut-tiket.php
+++ halaman/antrian/urut-tiket.php
@@ -2,5 +2,45 @@
 include 'halaman/_bagian/link.php';
 ?>
 Ini adalah halaman urutan tiket. ambil tiket <a href="?halaman=ambil-tiket">disini</a>
+<hr>
+Urutan Tiket
+
+No. Tiket :
+
+<a href="?halaman"></a>
+
+
+
+<hr>
+Daftar Tiket Antrian :
+<br>
+
+<table>
+    <tr>
+        <th>Urutan</th>
+        <th>Status</th>
+        <th>Waktu Ambil</th>
+        <th>Waktu Terpanggil</th>
+    </tr>
+    <?php
+    $tanggal = date("Y-m-d");
+
+    $query = $db->query("SELECT * FROM tiket_antrian WHERE hari = '{$tanggal}'");
+
+    while ($data = $query->fetch_object()) {
+    ?>
+        <tr>
+            <td><?= $data->nomor ?></td>
+            <td><?= $data->status_tiket ?></td>
+            <td><?= $data->tanggal_ambil ?></td>
+            <td><?= $data->tanggal_terpanggil ?></td>
+        </tr>
+    <?php
+    }
+    ?>
+</table>
+
+
+
 <?php
 include 'halaman/_bagian/footer.php';
