72a37cd5c804a543996d26dd860e0d0f34b922ea
sultanroomcode <sultanroomcode@gmail.com>
2021-05-10 21:17:15 +0700

    fungsi update status tiket

--- halaman/antrian/tiket-selesai.php
+++ halaman/antrian/tiket-selesai.php
@@ -1,6 +1,16 @@
 <?php
-include 'halaman/_bagian/link.php';
-?>
-Ini adalah halaman tiket selesai
-<?php
-include 'halaman/_bagian/footer.php';
+// ambil data tanggal
+$id_tiket = (int) $_GET['id'];
+$hasil = $db->query("UPDATE `tiket_antrian` SET `status_tiket` = 'SELESAI', `tanggal_terpanggil` = NOW() WHERE id = {$id_tiket}; ");
+
+if ($db->error) {
+    echo "Gagal eksekusi : ";
+    var_dump($db->error);
+    exit();
+}
+
+if ($hasil) {
+    header('Location: ?halaman=antrian-tiket&status=sukses');
+} else {
+    header('Location: ?halaman=antrian-tiket&status=gagal');
+}
